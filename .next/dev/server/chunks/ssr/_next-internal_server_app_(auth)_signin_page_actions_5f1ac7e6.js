ae0e9815361c0c5a12fb7acb427c333b8c304802
Volodymyr <volodymyr.shcherbatyy@gmail.com>
2026-03-22 19:38:09 +0100

    remove .next from git

--- .next/dev/server/chunks/ssr/_next-internal_server_app_(auth)_signin_page_actions_5f1ac7e6.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/(auth)/signin/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%28auth%29_signin_page_actions_5f1ac7e6.js.map
--- .next/dev/static/chunks/src_app_(auth)_signin_page_tsx_1cf6b850._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f3449fa9._.js"
-],
-    source: "dynamic"
-});
