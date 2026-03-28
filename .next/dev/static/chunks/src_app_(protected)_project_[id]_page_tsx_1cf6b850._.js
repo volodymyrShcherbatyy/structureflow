ca7c1d5c34fed5d7b17f880072d9e6589d44fe98
Volodymyr <volodymyr.shcherbatyy@gmail.com>
2026-03-28 15:54:56 +0100

    Remove .next from repository

--- .next/dev/static/chunks/src_app_(protected)_project_[id]_page_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_3abc6d79._.js",
-  "static/chunks/node_modules_025fd908._.js",
-  "static/chunks/node_modules_@xyflow_react_dist_style_6d3bc72a.css"
-],
-    source: "dynamic"
-});
